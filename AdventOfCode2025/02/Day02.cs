c2ec0d5b112f19547faf2d6c3b17791800576de8
Simon Wögerbauer <s.woegerbauer@students.htl-leonding.ac.at>
2025-12-02 06:26:50 +0100

    Day 02

--- AdventOfCode2025/02/Day02.cs
+++ AdventOfCode2025/02/Day02.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2024;
+﻿using System.Numerics;
+
+namespace AdventOfCode2024;
 
 public static class Day02
 {
@@ -14,17 +16,88 @@ public static class Day02
         PartTwo(false, input);
     }
 
+    private static bool IsRepeated(long number, bool requireExactTwo)
+    {
+        var numberString = number.ToString();
+        int length = numberString.Length;
+        if (length < 2) return false;
+        if (numberString[0] == '0') return false;
+
+        if (requireExactTwo)
+        {
+            if (length % 2 != 0) return false;
+            int half = length / 2;
+            return numberString.Substring(0, half).SequenceEqual(numberString.Substring(half, half));
+        }
+
+        for (int partLength = 1; partLength <= length / 2; partLength++)
+        {
+            if (length % partLength != 0) continue;
+            int repeats = length / partLength;
+            if (repeats < 2) continue;
+
+            var first = numberString.Substring(0, partLength);
+            bool allMatch = true;
+            for (int i = 1; i < repeats; i++)
+            {
+                if (!first.SequenceEqual(numberString.Substring(i * partLength, partLength)))
+                {
+                    allMatch = false;
+                    break;
+                }
+            }
+
+            if (allMatch) return true;
+        }
+
+        return false;
+    }
+
     private static void PartOne(bool isTest, string[] input)
     {
-        var result = 0;
-        
-        InputOutputHelper.WriteOutput(isTest, result);
+        long total = 0;
+
+        foreach (var line in input)
+        {
+            var ranges = line.Split(',');
+            foreach (var range in ranges)
+            {
+                var parts = range.Split('-');
+                if (parts.Length != 2) continue;
+                if (!long.TryParse(parts[0], out var start)) continue;
+                if (!long.TryParse(parts[1], out var end)) continue;
+
+                for (long i = start; i <= end; i++)
+                {
+                    if (IsRepeated(i, requireExactTwo: true)) total += i;
+                }
+            }
+        }
+
+        InputOutputHelper.WriteOutput(isTest, total.ToString());
     }
-    
+
     private static void PartTwo(bool isTest, string[] input)
     {
-        var result = 0;
-        
-        InputOutputHelper.WriteOutput(isTest, result);
+        long total = 0;
+
+        foreach (var line in input)
+        {
+            var ranges = line.Split(',');
+            foreach (var range in ranges)
+            {
+                var parts = range.Split('-');
+                if (parts.Length != 2) continue;
+                if (!long.TryParse(parts[0], out var start)) continue;
+                if (!long.TryParse(parts[1], out var end)) continue;
+
+                for (long i = start; i <= end; i++)
+                {
+                    if (IsRepeated(i, requireExactTwo: false)) total += i;
+                }
+            }
+        }
+
+        InputOutputHelper.WriteOutput(isTest, total.ToString());
     }
 }
