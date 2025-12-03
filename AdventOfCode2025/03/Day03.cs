cbf8b0faa7426d16160c850161099a5a3b2416c0
Simon Wögerbauer <s.woegerbauer@students.htl-leonding.ac.at>
2025-12-03 06:23:53 +0100

    Day 03

--- AdventOfCode2025/03/Day03.cs
+++ AdventOfCode2025/03/Day03.cs
@@ -13,17 +13,59 @@ public static class Day03
         PartTwo(true, testInput);
         PartTwo(false, input);
     }
+    
+    static long MaxSubsequenceValue(string sequence, int digits)
+    {
+        int length = sequence.Length;
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < length; i++)
+        {
+            char c = sequence[i];
+            while (stack.Count > 0 && stack.Peek() < c && stack.Count - 1 + (length - i) >= digits)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count < digits)
+            {
+                stack.Push(c);
+            }
+        }
+
+        long result = 0;
+        long factor = 1;
+        while (stack.Count > 0)
+        {
+            result += int.Parse(stack.Pop().ToString()) * factor;
+            factor *= 10;
+        }
+
+        return result;
+    }
+
+
 
     private static void PartOne(bool isTest, string[] input)
     {
-        var result = 0;
+        long result = 0;
+
+        foreach (var line in input)
+        {
+            result += MaxSubsequenceValue(line, 2);
+        }
         
         InputOutputHelper.WriteOutput(isTest, result);
     }
     
     private static void PartTwo(bool isTest, string[] input)
     {
-        var result = 0;
+        long result = 0;
+
+        foreach (var line in input)
+        {
+            result += MaxSubsequenceValue(line, 12);
+        }
         
         InputOutputHelper.WriteOutput(isTest, result);
     }
