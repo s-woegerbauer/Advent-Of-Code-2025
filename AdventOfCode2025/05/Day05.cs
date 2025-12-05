41f18437ace9b6a9771be32d3f9048f0b2caf8cd
Simon Wögerbauer <s.woegerbauer@students.htl-leonding.ac.at>
2025-12-05 06:13:00 +0100

    Day 05

--- AdventOfCode2025/05/Day05.cs
+++ AdventOfCode2025/05/Day05.cs
@@ -13,18 +13,73 @@ public static class Day05
         PartTwo(true, testInput);
         PartTwo(false, input);
     }
+    
+    public static (List<(long Start, long End)> Ranges, List<long> Ids) Parse(string[] lines)
+    {
+        var ranges = new List<(long Start, long End)>();
+        var ids = new List<long>();
+        long i = 0;
+
+        for (; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            if (string.IsNullOrEmpty(line))
+            {
+                i++; 
+                break;
+            }
+            var parts = line.Split('-');
+            if (parts.Length == 2 && long.TryParse(parts[0], out var a) && long.TryParse(parts[1], out var b))
+            {
+                ranges.Add((a, b));
+            }
+        }
+        
+        for (; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            if (long.TryParse(line, out var id)) ids.Add(id);
+        }
+
+        return (ranges, ids);
+    }
 
     private static void PartOne(bool isTest, string[] input)
     {
-        var result = 0;
+        long result = 0;
+        
+        var (ranges, ids) = Parse(input);
+
+        foreach (var id in ids)
+        {
+            if(ranges.Any(r => id >= r.Start && id <= r.End)) result++;
+        }
         
         InputOutputHelper.WriteOutput(isTest, result);
     }
     
     private static void PartTwo(bool isTest, string[] input)
     {
-        var result = 0;
+        long result = 0;
         
+        var (ranges, _) = Parse(input);
+
+        var mergedRanges = new List<(long Start, long End)>();
+        foreach (var range in ranges.OrderBy(r => r.Start))
+        {
+            if (mergedRanges.Count == 0 || mergedRanges.Last().End < range.Start - 1)
+            {
+                mergedRanges.Add(range);
+            }
+            else
+            {
+                var last = mergedRanges.Last();
+                mergedRanges[^1] = (last.Start, Math.Max(last.End, range.End));
+            }
+        }
+        
+        result = mergedRanges.Sum(mr => mr.End - mr.Start + 1);
+
         InputOutputHelper.WriteOutput(isTest, result);
     }
 }
