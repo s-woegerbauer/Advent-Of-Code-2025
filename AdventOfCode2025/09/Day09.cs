333b301dec3101d6c1f5c331e2d0666143826f24
Simon Wögerbauer <s.woegerbauer@students.htl-leonding.ac.at>
2025-12-09 06:38:06 +0100

    Day 09

--- AdventOfCode2025/09/Day09.cs
+++ AdventOfCode2025/09/Day09.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2024;
+﻿using NetTopologySuite.Geometries;
+
+namespace AdventOfCode2024;
 
 public static class Day09
 {
@@ -16,15 +18,63 @@ public static class Day09
 
     private static void PartOne(bool isTest, string[] input)
     {
-        var result = 0;
+        long result = 0;
+        
+        List<(long row, long col)> points = input.Select(line =>
+        {
+            var parts = line.Split(',');
+            return (long.Parse(parts[0]), long.Parse(parts[1]));
+        }).ToList();
+
+        foreach (var coord1 in points)
+        {
+            foreach (var coord2 in points)
+            {
+                if(coord1 == coord2) continue;
+                
+                long area = Math.Abs(coord1.row - coord2.row + 1) * Math.Abs(coord1.col - coord2.col + 1);
+                result = Math.Max(result, area);
+            }
+        }
         
         InputOutputHelper.WriteOutput(isTest, result);
     }
     
     private static void PartTwo(bool isTest, string[] input)
     {
-        var result = 0;
-        
-        InputOutputHelper.WriteOutput(isTest, result);
+        List<(long row, long col)> points = input.Select(line =>
+        {
+            var parts = line.Split(',');
+            return (long.Parse(parts[0]), long.Parse(parts[1]));
+        }).ToList();
+
+        var coords = new Coordinate[points.Count + 1];
+        for (int i = 0; i < points.Count; i++)
+        {
+            coords[i] = new Coordinate(points[i].col, points[i].row);
+        }
+        coords[points.Count] = coords[0];
+
+        long res = 0;
+        var factory = new GeometryFactory();
+        var poly = factory.CreatePolygon(coords);
+
+        var ring = coords.Take(coords.Length - 1).ToArray();
+        foreach (var (a, ai) in ring.Select((c, idx) => (c, idx)))
+        {
+            foreach (var (b, bi) in ring.Select((c, idx) => (c, idx)))
+            {
+                if (ai == bi) continue;
+                var envelope = new Envelope(a, b);
+                var subset = factory.ToGeometry(envelope);
+                if (subset.Within(poly))
+                {
+                    var area = (long)((Math.Abs(b.X - a.X) + 1) * (Math.Abs(b.Y - a.Y) + 1));
+                    if (area > res) res = area;
+                }
+            }
+        }
+
+        InputOutputHelper.WriteOutput(isTest, res);
     }
 }
